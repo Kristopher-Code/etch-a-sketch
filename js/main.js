03f4d5e9097218e41772dec1d444fae9c904e8e4
Kristopher-Code <knicholcode@gmail.com>
2022-07-16 22:11:38 +0100

    Added grid resizing and clearing functionality. Changed the mouse onclick event.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
 // Grid container.
-const container = document.querySelector('.container');
+const container = document.getElementById("container");
+const clear = document.getElementById("clearBtn")
+const size = document.getElementById("sizeChangeBtn")
 let gridSize = 16;
 
-// Add 16 divs.
+let mouseDown = false;
+
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
+
+clearBtn.onclick = () => clearGrid();
+sizeChangeBtn.onclick = () => changeGridSize();
+    // Change colour on mouse hover.
+
+    changeColour = (e) => {
+        if (e.type === "mouseover" && !mouseDown) return;
+        e.target.style.backgroundColor = "white";
+    }
+
+changeGridSize = () => {
+    let newSize = (prompt("Enter a number"));
+    gridSize = newSize;
+    container.innerHTML = "";
+    createGrid(newSize);
+}
+
+clearGrid = () => {
+    container.innerHTML = "";
+    createGrid(gridSize);
+}
 
 createGrid = () => {
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
     for (let i = 0; i < gridSize * gridSize; i++) {
-        const grid = document.createElement('div');
-        grid.style.cssText = "border: 1px solid black;";        
+        const grid = document.createElement("div");
+        grid.classList.add("grid-class");
+        grid.addEventListener("mouseover", changeColour);
+        grid.addEventListener("mousedown", changeColour);
         container.appendChild(grid);
-        container.addEventListener("mouseover", changeColour);
-        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     }
 };
 
-// Change colour on mouse hover.
 
-changeColour = (e) => {
-    e.target.style.backgroundColor = "pink";
-}
 
 createGrid(gridSize);
+
 
